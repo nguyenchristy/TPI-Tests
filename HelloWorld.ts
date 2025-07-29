17bff779db8ee5a8e3b4cc5d9ae9c95f0a56763b
Christy <t-nguyenchr@microsoft.com>
2025-07-29 14:31:24 -0700

    Remove HelloWorld.js and HelloWorld.ts files

--- HelloWorld.ts
+++
@@ -1,9 +0,0 @@
-const message = "hello world";
-console.log(message);
-
-// Count characters using a for loop
-let characterCount = 0;
-for (let i = 0; i < message.length; i++) {
-    characterCount++;
-}
-console.log(`Number of characters: ${characterCount}`);
