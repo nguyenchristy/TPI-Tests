5c9679935c61995efd3b7056895c40e05e675c41
Christy <t-nguyenchr@microsoft.com>
2025-07-29 09:58:19 -0700

    Add character counting for loop to Hello World programs

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,13 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        String message = "hello world";
+        System.out.println(message);
+        
+        // Count characters using a for loop
+        int characterCount = 0;
+        for (int i = 0; i < message.length(); i++) {
+            characterCount++;
+        }
+        System.out.println("Number of characters: " + characterCount);
     }
 }
--- HelloWorld.js
+++ HelloWorld.js
@@ -1,1 +1,9 @@
-console.log("Hello World");
+const message = "hello world";
+console.log(message);
+
+// Count characters using a for loop
+let characterCount = 0;
+for (let i = 0; i < message.length; i++) {
+    characterCount++;
+}
+console.log(`Number of characters: ${characterCount}`);
